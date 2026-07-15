a2c0cd9a2316fb84c1a05ddbde0712e022104da6
Andrew <nimandrey22@gmail.com>
2026-07-15 20:44:38 +0900

    feat: add init_snake function

--- main.c
+++ main.c
@@ -2,11 +2,21 @@
 #define WIDTH 20
 #define HEIGHT 10
 
+struct Point {
+    int x;
+    int y;
+};
+
+struct Point snake[100];
+int snake_length;
+
 void draw_field() {
     for(int i = 0; i < HEIGHT; i++) {
         for(int j = 0; j < WIDTH; j++) {
             if ((i == 0 || i == HEIGHT - 1) || (j == 0 || j == WIDTH - 1))  {
                 putchar('#');
+            } else if (i == snake[0].y && j == snake[0].x) {
+                putchar('O');
             } else {
                 printf(" ");
             }
@@ -15,7 +25,14 @@ void draw_field() {
     }
 }
 
+void init_snake() {
+    snake[0].x = WIDTH / 2;
+    snake[0].y = HEIGHT / 2;
+    snake_length = 1;
+}
+
 int main() {
+    init_snake();
     draw_field();
     return 0;
 }
